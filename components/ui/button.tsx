caa7127f65e7d9b5dd3d07bdaa0a2b4d0f656bcb
Taylor Ho <taylorkmho@gmail.com>
2024-04-03 12:35:31 -0700

    improve layout for all states
    - add ScrollArea
    - add variant to Button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -29,6 +29,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        secondaryBrand: "bg-primary/15 text-primary hover:bg-primary/30",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
--- pages/join/thank-you.tsx
+++ pages/join/thank-you.tsx
@@ -3,14 +3,14 @@ import Logo from "@/components/Logo";
 import MetaTags from "@/components/Metatags";
 import Plausible from "@/components/Plausible";
 import { Subtitle } from "@/components/Title";
-import { Computer, MessageCircleHeart } from "lucide-react";
+import { ArrowUpRight, Computer, MessageCircleHeart } from "lucide-react";
 import Head from "next/head";
 import Link from "next/link";
-import { DISCORD_URL } from "../about";
+import { DISCORD_URL, GITHUB_URL } from "../about";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { Filter, getFilters } from "@/lib/api";
-import { FirebaseTablesEnum } from "@/lib/enums";
+import { FirebaseTablesEnum, StatusEnum } from "@/lib/enums";
 import { getMembers } from "../api/get-members";
 import {
   Accordion,
@@ -20,7 +20,8 @@ import {
 } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
 import theme from "@/styles/theme";
-import { Badge } from "@/components/ui/badge";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { buttonVariants } from "@/components/ui/button";
 
 export async function getStaticProps() {
   const { members, focuses, industries } = await getMembers();
@@ -33,14 +34,18 @@ export async function getStaticProps() {
       members: members,
       focuses: await getFilters(
         FirebaseTablesEnum.FOCUSES,
-        false,
-        undefined,
+        true,
+        members
+          .filter((member) => member.status === StatusEnum.APPROVED)
+          .map((m) => m.id),
         focuses,
       ),
       industries: await getFilters(
         FirebaseTablesEnum.INDUSTRIES,
-        false,
-        undefined,
+        true,
+        members
+          .filter((member) => member.status === StatusEnum.APPROVED)
+          .map((m) => m.id),
         industries,
       ),
     },
@@ -108,78 +113,155 @@ export default function ThankYou({ pageTitle, focuses, industries, members }) {
             and get you added to the directory. Beyond that, this is a pretty
             (intentionally) simple operation. 🤙🏼🤙🏽🤙🏾
           </p>
-          <section className="grid grid-cols-2 gap-4">
-            <div>
-              <div className="flex flex-col rounded-xl bg-brown-600/10 pb-2 pt-4">
-                <div className="space-y-2 px-4">
+          <section className="flex w-full justify-stretch gap-4">
+            <div className="grow">
+              <div className="flex h-full flex-col justify-between rounded-xl bg-brown-600/10 pb-4 pt-4">
+                <div className="mb-2 space-y-2 px-4">
                   <Computer style={{ color: theme.color.link.base }} />
                   <h3 className="font-semibold text-foreground">
                     Connect with your kanaka peers in common.
                   </h3>
                 </div>
+
                 {(similarFocuses.length > 0 ||
                   similarIndustries.length > 0) && (
                   <Accordion type="single" collapsible>
                     {[similarFocuses, similarIndustries].map((items, i) => {
                       if (items.length === 0) return null;
+
                       return (
                         <div key={`${items[0].filterType}-${i}`}>
                           {items.length > 0 && (
-                            <h6 className="mb-1 mt-2 px-4 text-xs font-semibold uppercase tracking-wide text-brown-600">
+                            <h6 className="px-4 py-2 text-xs font-semibold uppercase tracking-wide text-brown-600">
                               {items[0].filterType}
                             </h6>
                           )}
-                          {items.map((item, itemIndex) => (
-                            <AccordionItem
-                              key={`filter-${itemIndex}-${item.name}`}
-                              value={`filter-${itemIndex}-${item.name}`}
-                              className={cn(
-                                items.length === itemIndex + 1 && "border-none",
-                              )}
-                            >
-                              <AccordionTrigger className="border-0 px-4 hover:bg-brown-600/10">
-                                <div className="flex items-center">
+                          {items.map((item, itemIndex) => {
+                            // conditionally add scroll area if there are more than 8 members
+                            let memberContent = (
+                              <div className="grid grid-cols-2 gap-1 py-1 pl-5 pr-2">
+                                {/* TODO
+                                    - this sort of should probably be made possible when we update get-members
+                                    - return the members with the filters
+                                */}
+                                {members
+                                  .filter(
+                                    (member) =>
+                                      (items[0].filterType === "focuses" &&
+                                        member.focus
+                                          ?.map((foc) => foc.id)
+                                          .includes(item.id)) ||
+                                      (items[0].filterType === "industries" &&
+                                        member.industry
+                                          ?.map((foc) => foc.id)
+                                          .includes(item.id)),
+                                  )
+                                  .map((member) => (
+                                    <Link
+                                      href={member.link}
+                                      className="flex items-center p-1 text-xs text-foreground"
+                                      target="_blank"
+                                    >
+                                      <div className="grow">
+                                        <p className="line-clamp-1 font-semibold">
+                                          {member.name}
+                                        </p>
+                                        <p className="line-clamp-1">
+                                          {member.title}
+                                        </p>
+                                      </div>
+                                      <ArrowUpRight className="h-5 w-5 shrink-0 text-primary" />
+                                    </Link>
+                                  ))}
+                              </div>
+                            );
+
+                            if (items[itemIndex].count > 8) {
+                              memberContent = (
+                                <ScrollArea className="h-48 w-full grid-cols-2 gap-2 rounded-md">
+                                  {memberContent}
+                                </ScrollArea>
+                              );
+                            }
+
+                            return (
+                              <AccordionItem
+                                key={`filter-${itemIndex}-${item.name}`}
+                                value={`filter-${itemIndex}-${item.name}`}
+                              >
+                                <AccordionTrigger className="border-0 px-4 hover:bg-brown-600/10">
+                                  <div className="flex items-center">
+                                    {/* TODO
+                                      - this count is inaccurate, but I want to use it!
                                   <div className="flex min-w-12">
                                     <Badge variant="ghost">{item.count}</Badge>
+                                  </div> */}
+                                    <h3 className="line-clamp-1 text-left">
+                                      {item.name}
+                                    </h3>
                                   </div>
-                                  <h3 className="line-clamp-1 text-left">
-                                    {item.name}
-                                  </h3>
-                                </div>
-                              </AccordionTrigger>
-                              <AccordionContent className="px-4">
-                                Placeholder
-                              </AccordionContent>
-                            </AccordionItem>
-                          ))}
+                                </AccordionTrigger>
+                                <AccordionContent>
+                                  {memberContent}
+                                </AccordionContent>
+                              </AccordionItem>
+                            );
+                          })}
                         </div>
                       );
                     })}
                   </Accordion>
                 )}
+                <div className="mt-2 px-4">
+                  <Link
+                    href="/?utm_source=thank-you"
+                    target="_blank"
+                    className={cn(
+                      buttonVariants({ variant: "secondaryBrand" }),
+                      "w-full justify-start",
+                    )}
+                  >
+                    → Home
+                  </Link>
+                </div>
               </div>
             </div>
-            <div className="grid grid-rows-2 gap-4">
-              <Link
-                href="https://hawaiiansintech.org/discord"
-                className="flex flex-col gap-2 rounded-xl bg-brown-600/10 p-2 sm:p-4"
-              >
-                <MessageCircleHeart />
-                <h3 className="font-semibold text-foreground">
-                  Join the discussion on our Discord server.
-                </h3>
-                <span>→ Discord</span>
-              </Link>
-              <Link
-                href={`${DISCORD_URL}`}
-                className="flex flex-col gap-2 rounded-xl bg-brown-600/10 p-2 sm:p-4"
-              >
-                <Computer />
-                <h3 className="font-semibold text-foreground">
-                  Contribute to our projects on GitHub.
-                </h3>
-                <span>→ Github</span>
-              </Link>
+            <div className="flex w-1/3 shrink-0 flex-col justify-start gap-4">
+              {[
+                {
+                  icon: <MessageCircleHeart />,
+                  title: "Join the discussion on our Discord server.",
+                  link: DISCORD_URL,
+                  linkLabel: "Discord",
+                },
+                {
+                  icon: <Computer />,
+                  title: "Contribute to our projects on GitHub.",
+                  link: GITHUB_URL,
+                  linkLabel: "Github",
+                },
+              ].map((item, index) => {
+                const { icon, title, link, linkLabel } = item;
+                return (
+                  <div
+                    className="flex flex-col gap-2 rounded-xl bg-brown-600/10 p-2 text-primary sm:p-4"
+                    key={`link-${index}`}
+                  >
+                    {icon}
+                    <h3 className="font-semibold text-foreground">{title}</h3>
+                    <Link
+                      href={`${link}?utm_source=thank-you`}
+                      target="_blank"
+                      className={cn(
+                        buttonVariants({ variant: "secondaryBrand" }),
+                        "w-full justify-start",
+                      )}
+                    >
+                      → {linkLabel}
+                    </Link>
+                  </div>
+                );
+              })}
             </div>
           </section>
           <p className="text-sm tracking-wide text-secondary-foreground">
