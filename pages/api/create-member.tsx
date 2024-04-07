e79d8c18448835f6cb5cdb76b6633f39576860c0
Taylor Ho <taylorkmho@gmail.com>
2024-04-07 11:56:21 -0700

    removed console.log

--- pages/api/create-member.tsx
+++ pages/api/create-member.tsx
@@ -198,8 +198,6 @@ export default async function handler(req, res) {
       title,
       website, // TODO: Remove "website" input param and replace with "link"
     } = req.body;
-    console.log(req.body);
-
     const isEmailUsed = await emailExists(email);
     if (isEmailUsed) {
       console.log("🚫 email already exists");
