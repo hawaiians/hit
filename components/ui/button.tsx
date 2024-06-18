1514e28db14f4017e2e2bba2245d33bd69418702
Taylor Ho <taylorkmho@gmail.com>
2024-06-17 22:22:59 -0700

    update <Button />

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,13 +3,13 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import LoadingSpinner, { LoadingSpinnerVariant } from "../LoadingSpinner";
 
 const buttonVariants = cva(
   `inline-flex
   items-center
   justify-center
   rounded-md
-  text-sm
   font-medium
   ring-offset-background
   transition-colors
@@ -23,21 +23,22 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground",
+          "bg-primary text-primary-foreground hover:bg-primary/80 active:bg-primary/90 hover:text-primary-foreground",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input/50 bg-background hover:border-input border-2",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/70 hover:text-secondary-foreground",
+          "bg-secondary text-secondary-foreground hover:text-secondary-foreground hover:bg-secondary/80",
         secondaryBrand: "bg-primary/15 text-primary hover:bg-primary/30",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        noAnim: "hover:scale-100 active:scale-100",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        sm: "h-10 rounded py-1 px-2",
+        default: "h-12 rounded-sm p-2 text-xl sm:text-base",
+        lg: "h-14 rounded-md p-4",
         icon: "h-10 w-10",
       },
     },
@@ -52,17 +53,37 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      children,
+      variant,
+      size,
+      asChild = false,
+      loading = false,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {loading ? (
+          <>
+            <LoadingSpinner />
+          </>
+        ) : (
+          <>{children}</>
+        )}
+      </Comp>
     );
   },
 );
